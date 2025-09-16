6a0ffdbb71c733f687b5e7fba1a765df8aa32dd4
Trungkiett <trungkiet102@gmail.com>
2025-09-16 15:52:02 +0700

    Lấy toàn bộ folder farmer trong pages từ khoitran09

---
+++ src/pages/farmer/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/farmer/Header";
+  
+const DashboardFarmer =()=>{
+  return(
+    <div>
+      <Header />
+       {/* <Main /> */}
+      {/* <MenuAppBar /> */}
+       {/* <Footer /> */}
+     </div>
+  );
+}
+export default DashboardFarmer;
