7ea22ef8d9f825a2c38bb29237fce45090018f32
khoitrants03 <trankhoi671@gmail.com>
2025-09-17 15:39:57 +0700

    thư mục BE_management

---
+++ src/be_management/controller/components/connect.php
@@ -0,0 +1,10 @@
+<?php
+$db_name = 'mysql:host=127.0.0.1:4306;dbname=quanlinongtrai_kl;charset=utf8mb4'; 
+// $db_name = 'mysql:host=127.0.0.1;dbname=quan_li_bv;charset=utf8mb4';
+$user_name = 'root';
+$user_password = '';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+ 
+?>
